6e2b19952027e711508ff35e21f7f5a5d3abecb6
程飞 <chengfei@newland.com.cn>
2022-04-18 17:40:15 +0800

    静态工厂方法替代构造方法，多个参数的时候，利用builder的方法比较好

---
+++ java-advanced-features/src/main/java/effective/Item01.java
@@ -0,0 +1,22 @@
+package effective;
+
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumSet;
+
+/**
+ * @ClassName: Item01
+ * @Author chengfei
+ * @DateTime 2022/4/7 8:42
+ * @todo: 静态工厂方法替代构造方法
+ **/
+public class Item01 {
+    public static void main(String[] args) {
+        Boolean aTrue = Boolean.valueOf("true");
+        Boolean bTrue = Boolean.valueOf(false);
+//        BigInteger.probablePrime()
+//        EnumSet.of()
+        System.out.println("s");
+    }
+}
