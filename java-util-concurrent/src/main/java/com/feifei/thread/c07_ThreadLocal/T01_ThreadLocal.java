1a96519a77a79109680aa1981a5572228d574ae0
feifei <xingchendahai_fei@163.com>
2022-05-07 18:01:07 +0800

    完善threadlocal例子

--- java-util-concurrent/src/main/java/com/feifei/thread/c07_ThreadLocal/T01_ThreadLocal.java
+++ java-util-concurrent/src/main/java/com/feifei/thread/c07_ThreadLocal/T01_ThreadLocal.java
@@ -3,7 +3,7 @@ package com.feifei.thread.c07_ThreadLocal;
 import java.util.concurrent.TimeUnit;
 
 /**
- * @Description:  各个线程是独立的，所有在一个线程里设置，在另外一个线程里是不能读到的，代码结果就是null
+ * @Description: 各个线程是独立的，所有在一个线程里设置，在另外一个线程里是不能读到的，代码结果就是null
  * @ClassName: T01_ThreadLocal
  * @Author chengfei
  * @DateTime 2021/5/18 18:17
@@ -16,27 +16,32 @@ public class T01_ThreadLocal {
 
     public static void main(String[] args) {
 
-        new Thread(()->{
+        Thread thread1 = new Thread(() -> {
             try {
-                TimeUnit.SECONDS.sleep(2);
+                TimeUnit.SECONDS.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            System.out.println(String.format("%s->%s", Thread.currentThread().getName(), tl.get()));
+//            System.out.println(Thread.currentThread().getName() + tl.get() + " ");
+        });
+        thread1.setName("one thread");
+        thread1.start();
 
-            System.out.println(tl.get()+ " " );
-        }).start();
-
-        new Thread(()->{
+        Thread thread2 = new Thread(() -> {
 //            try {
 //                TimeUnit.SECONDS.sleep(1);
 //            } catch (InterruptedException e) {
 //                e.printStackTrace();
 //            }
             tl.set(new Person());
-            System.out.println(tl.get()+ " " + tl.get().name);
-        }).start();
+            System.out.println(String.format("%s->%s->%s", Thread.currentThread().getName(), tl.get(), tl.get().name));
+        });
+        thread2.setName("two thread");
+        thread2.start();
     }
 }
+
 class Person {
     String name = "zhangsan";
 }
